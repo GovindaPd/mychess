ee43a221eb8032de3cff39fe907b193d85cb3fb3
GovindaPd <91777257+GovindaPd@users.noreply.github.com>
2024-02-18 12:50:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,3 +6,4 @@ pm_color = (.0,.8,.8,1)
 gui_m = {'WE': 'r', 'WK': 'k', 'WQ': 'q', 'WC': 'b', 'WH': 'h', 'WS': 'p', 'BE': 't', 'BK': 'l', 'BQ': 'w', 'BC': 'n', 'BH': 'j', 'BS': 'o'}
 game_level = {1:2, 2:3, 3:4}
 man_color = {'W':(1,1,1,1), 'B':(0,0,0,1)}
+background = (.168,.168,.168,1)
